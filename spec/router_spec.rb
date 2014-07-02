f308f67c945424a6ec5490f3cac0a55f49f78d02
Emmett Faricy <emmett.faricy@gmail.com>
2014-07-01 17:07:26 -0700

    Phase VIe

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -21,7 +21,7 @@ describe Route do
       index_route.matches?(req).should be_true
     end
 
-    it "correctly doesn't matche regular expression with capture" do
+    it "correctly doesn't match regular expression with capture" do
       index_route = Route.new(Regexp.new("^/users/(?<id>\\d+)$"), :get, "UsersController", :index)
       req.stub(:path) { "/statuses/1" }
       req.stub(:request_method) { :get }
